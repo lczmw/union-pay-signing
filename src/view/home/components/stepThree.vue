4067b627d5d33ad0b780671e149bff16c5804bfc
zhangmowen <lczmw@126.com>
2020-01-03 17:49:50 +0800

    打开对公账户认证入口

--- src/view/home/components/stepThree.vue
+++ src/view/home/components/stepThree.vue
@@ -69,7 +69,10 @@
         </van-field>
       </van-cell-group>
 
-      <ul v-if="accountName.type == 1" class="home-tip fz12">
+      <ul
+        v-if="accountName.type == 1"
+        class="home-tip fz12"
+      >
         <li>
           <span class="dot">●</span>个人账户认证：根据姓名、身份证号、银行卡号三要素和银行开户信息匹配一致则认为为本人。
         </li>
@@ -77,7 +80,10 @@
           <span class="dot">●</span>多关键词查询以“/”符号分隔，需要遵从前后顺序对应规则，如“招商/东方”
         </li>
       </ul>
-      <ul v-else class="home-tip fz12">
+      <ul
+        v-else
+        class="home-tip fz12"
+      >
         <li>
           <span class="dot">●</span>通过搜索有效关键字快速定位支行信息，如“中国招商银行上海东方支行”，通过“东方”有效关键字快速定位支行。
         </li>
@@ -86,38 +92,65 @@
         </li>
       </ul>
 
-      <section v-show="accountName.type == 1" class="home-upload">
+      <section
+        v-show="accountName.type == 1"
+        class="home-upload"
+      >
         <div class="home-upload-txt">
           法人手持身份证自拍照
           <span>(图片支持jpg、png、bmg格式，文件不超过2M)</span>
         </div>
         <div class="home-upload-content">
-          <Upload :disabled="threeDisabled" type="4" document-type="0007" not-uploaded-tip="请上传手持身份证自拍照" ref="upload0"/>
+          <Upload
+            :disabled="threeDisabled"
+            type="4"
+            document-type="0007"
+            not-uploaded-tip="请上传手持身份证自拍照"
+            ref="upload0"
+          />
         </div>
       </section>
-      <section v-show="accountName.type == 2" class="home-upload">
+      <section
+        v-show="accountName.type == 2"
+        class="home-upload"
+      >
         <div class="home-upload-txt">
           开户许可证
           <span>(图片支持jpg、png、bmg格式，文件不超过2M)</span>
         </div>
         <div class="home-upload-content">
-          <Upload :disabled="threeDisabled" document-type="0006" not-uploaded-tip="请上传开户许可证" ref="upload1"/>
+          <Upload
+            :disabled="threeDisabled"
+            document-type="0006"
+            not-uploaded-tip="请上传开户许可证"
+            ref="upload1"
+          />
         </div>
       </section>
-     <!--  <ul v-show="accountName.type == 2" class="home-tip">
+      <ul
+        v-show="accountName.type == 2"
+        class="home-tip"
+      >
         <li>
           <span class="dot">●</span>我们将在您的对公账户打一笔随机金额和验证码用于验证
         </li>
       </ul>
 
-      <div v-show="accountName.type == 2" class="tab auth" @click="showAuth">
+      <div
+        v-show="accountName.type == 2"
+        class="tab auth"
+        @click="showAuth"
+      >
         <div class="tabTitle">对公账户认证</div>
         <div class="fr">
           去认证
-          <van-icon name="arrow"/>
+          <van-icon name="arrow" />
         </div>
-      </div> -->
-      <van-popup v-model="showArea" position="bottom">
+      </div>
+      <van-popup
+        v-model="showArea"
+        position="bottom"
+      >
         <Area
           :areaValue="accountName.country"
           :num="2"
@@ -125,25 +158,41 @@
           @confirmArea="confirmArea"
         />
       </van-popup>
-      <van-popup v-model="showBank" position="bottom">
-        <van-picker show-toolbar :columns="columns" @cancel="showBank=false" @confirm="onConfirm"/>
+      <van-popup
+        v-model="showBank"
+        position="bottom"
+      >
+        <van-picker
+          show-toolbar
+          :columns="columns"
+          @cancel="showBank=false"
+          @confirm="onConfirm"
+        />
       </van-popup>
     </div>
     <footer :style="footerHS?'':'position:relative'">
       <div class="doubleBtn">
-        <van-button type="warning" size="large" @click="goBack">上一步</van-button>
-        <van-button type="primary" size="large" @click="nextPage">下一步</van-button>
+        <van-button
+          type="warning"
+          size="large"
+          @click="goBack"
+        >上一步</van-button>
+        <van-button
+          type="primary"
+          size="large"
+          @click="nextPage"
+        >下一步</van-button>
       </div>
     </footer>
   </div>
 </template>
 
 <script>
-import Cookies from 'js-cookie'
-import { queryBranchBank, setBankInfo } from '@/api'
-import { getStore } from '@/utils'
-import Area from '@/components/Area'
-import Upload from '@/components/Upload'
+import Cookies from 'js-cookie';
+import { queryBranchBank, setBankInfo } from '@/api';
+import { getStore } from '@/utils';
+import Area from '@/components/Area';
+import Upload from '@/components/Upload';
 export default {
   components: {
     Area,
@@ -168,7 +217,7 @@ export default {
         branchName: '',
         branchCode: '',
         openName: '',
-        name: '',
+        name: ''
       },
       accountError: {
         type: false,
@@ -183,7 +232,7 @@ export default {
         // { text: '宁波', id: '2' },
         // { text: '温州', id: '3' }
       ]
-    }
+    };
   },
   methods: {
     queryBranchBank() {
@@ -192,33 +241,33 @@ export default {
           message: '请选择开户所在地',
           duration: 2000
         });
-        return
+        return;
       }
       if (this.accountName.branchName === '') {
         this.$toast({
           message: '请输入所属支行关键字',
           duration: 2000
         });
-        return
+        return;
       }
       queryBranchBank({
         cityId: this.city.code,
         key: this.accountName.branchName
       }).then(res => {
-        this.showBank = true
-        this.columns = []
+        this.showBank = true;
+        this.columns = [];
         res.result.branchBankList.map(item => {
           this.columns.push({
-            text: item.bankBranchName, id: item.code
-          })
-        })
-      })
+            text: item.bankBranchName,
+            id: item.code
+          });
+        });
+      });
     },
     onConfirm({ id, text }) {
-      this.showBank = false
-      this.accountName.branchName = text
-      this.accountName.branchCode = id
-  
+      this.showBank = false;
+      this.accountName.branchName = text;
+      this.accountName.branchCode = id;
     },
     init() {
       let signing = getStore('pospal_signing', true);
@@ -231,105 +280,103 @@ export default {
       this.initUploadStatus();
     },
     initPageData() {
-      
-       let { bank_acct_type, bank_acct_no, bank_branch_name, bank_no, bank_acct_name } = this.globalMixin_getSigning()
-        this.accountName.type = ~~bank_acct_type + 1;
-        this.accountName.name =  bank_acct_no;
-        this.accountName.branchName = bank_branch_name;
-        this.accountName.branchCode = bank_no;
-        this.accountName.openName = bank_acct_name;
+      let {
+        bank_acct_type,
+        bank_acct_no,
+        bank_branch_name,
+        bank_no,
+        bank_acct_name
+      } = this.globalMixin_getSigning();
+      this.accountName.type = ~~bank_acct_type + 1;
+      this.accountName.name = bank_acct_no;
+      this.accountName.branchName = bank_branch_name;
+      this.accountName.branchCode = bank_no;
+      this.accountName.openName = bank_acct_name;
     },
     confirmArea(data) {
-      
-      this.city = data[1]
-      this.accountName.addr = `${data[0].name} ${data[1].name}`
-      this.showArea = false
+      this.city = data[1];
+      this.accountName.addr = `${data[0].name} ${data[1].name}`;
+      this.showArea = false;
     },
-    
+
     showAreaFun() {
-      this.showArea = true
+      this.showArea = true;
     },
     hideArea() {
-      this.showArea = false
+      this.showArea = false;
     },
     onRead(file) {
-      console.log(file)
+      console.log(file);
     },
     showAuth() {
-      this.$emit('showAuth')
+      this.$emit('showAuth');
     },
     goBack() {
-      this.$emit('goBack')
+      this.$emit('goBack');
     },
     getParams() {
       let params = {
-        'sign_id': Cookies.get('sign_id'),
-        'bank_acct_type': ''+(this.accountName.type - 1),
-        'bank_acct_no': this.accountName.name,
-        'bank_branch_name': this.accountName.branchName,
-        'bank_no': this.accountName.branchCode,
-        'bank_acct_name': this.accountName.type === 2 ? this.accountName.openName : getStore('pospal_signing', true).legal_name
-      }
+        sign_id: Cookies.get('sign_id'),
+        bank_acct_type: '' + (this.accountName.type - 1),
+        bank_acct_no: this.accountName.name,
+        bank_branch_name: this.accountName.branchName,
+        bank_no: this.accountName.branchCode,
+        bank_acct_name:
+          this.accountName.type === 2
+            ? this.accountName.openName
+            : getStore('pospal_signing', true).legal_name
+      };
 
-      return params
+      return params;
     },
-    onFocus() {
-
-    },
+    onFocus() {},
     isImagesListUploaded() {
       let uploadIndexList = this.accountName.type == 1 ? ['0'] : ['1'];
       let notUploaded = uploadIndexList.some((item, index) => {
-        return this.$refs[`upload${ item }`].hasUploaded() === false;
-      })
+        return this.$refs[`upload${item}`].hasUploaded() === false;
+      });
       return !notUploaded;
     },
     initUploadStatus() {
-      ['0', '1'].forEach((item) => {
-        this.$refs[`upload${ item }`].init();
-      })
+      ['0', '1'].forEach(item => {
+        this.$refs[`upload${item}`].init();
+      });
     },
     nextPage() {
-
-      let params = this.getParams()
+      let params = this.getParams();
       let validField = {
-        'bank_acct_no': '请填写银行账号',
-        'bank_no': '请选择所属支行',
-        'bank_acct_name': '请填写开户名称',
-      }
+        bank_acct_no: '请填写银行账号',
+        bank_no: '请选择所属支行',
+        bank_acct_name: '请填写开户名称'
+      };
 
       this.globalMixin_validFormEmpty(validField, params)
-      .then(() => {
+        .then(() => {
+          if (!this.isImagesListUploaded()) {
+            return;
+          }
 
-        if (!this.isImagesListUploaded()) {
-          return
-        }
+          setBankInfo(params)
+            .then(({ result }) => {
+              this.globalMixin_updateSigning(result);
+              if (result.sign_step === 4) {
+                this.showAuth();
+              } else if (result.sign_step === 5) {
+                window.location = result.h5_agreement_sign_url;
+              }
+            })
 
-        setBankInfo(params)
-        .then(({ result }) => {
-         
-          this.globalMixin_updateSigning(result)
-           if (result.sign_step === 4) {
-            this.showAuth();
-           } else if (result.sign_step === 5) {
-             window.location = result.h5_agreement_sign_url
-           }
+            .catch(() => {});
         })
-
-        .catch(() => {})
-      })
-      .catch(() => {
-
-      })
-    },
+        .catch(() => {});
+    }
   },
-  created() {
-   
-  }
-}
+  created() {}
+};
 </script>
 
 <style lang="less" scoped>
-@import "../../../assect/style/var.less";
+@import '../../../assect/style/var.less';
 .fixedFooter {
   padding-bottom: 70px;
 }
